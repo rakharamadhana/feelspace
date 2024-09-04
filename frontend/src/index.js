867059c8b5a3891173ed8e795cddd41a6eb71b5f
Rakha Ramadhana <rakha.ramadhana@yahoo.com>
2024-09-05 03:06:39 +0800

    Make it into PWA

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'; // Import service worker registration
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
+
+// Register the service worker for offline support and faster load times
+serviceWorkerRegistration.register(); // Register the service worker
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
