0600f92693e7eb4ca4f6a620911e27af65d14238
Rakha Ramadhana <rakha.ramadhana@yahoo.com>
2024-12-19 01:51:29 +0800

    No distinction between Ctrl and Exp

--- frontend/src/pages/CardMaker.js
+++ frontend/src/pages/CardMaker.js
@@ -13,14 +13,14 @@ const CardMaker = () => {
                 <FadeIn>
                     <h1 className="text-6xl lg:text-8xl xl:text-9xl text-center font-bold mb-16">卡牌創作</h1>
                     <div className="flex space-x-6">
-                        {role === 'Student-Ctrl' && (
+                        {/*{role === 'Student-Ctrl' && (*/}
                         <Card imageUrl={`${process.env.PUBLIC_URL}/assets/image.png`} text="繪圖板"
                               borderColor="border-red-300" textColor="text-red-300" link="/card-maker/draw" simple={true}/>
-                        )}
-                        {role === 'Student-Exp' && (
+                        {/*)}*/}
+                        {/*{role === 'Student-Exp' && (*/}
                             <Card imageUrl={`${process.env.PUBLIC_URL}/assets/image.png`} text="AI工具"
                                   borderColor="border-blue-300" textColor="text-blue-300" link="https://www.adobe.com/tw/products/firefly.html" target="_blank" simple={true}/>
-                        )}
+                        {/*)}*/}
                         <Card imageUrl={`${process.env.PUBLIC_URL}/assets/image.png`} text="卡牌製作"
                               borderColor="border-green-300" textColor="text-green-300" link="/card-maker/create" simple={true}/>
                     </div>
